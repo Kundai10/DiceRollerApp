7b2f58e9e082d7c21d739c5276f00ad7e54abb70
Kundai Chasinda <chasindak9@gmail.com>
2022-12-30 19:16:24 +0200

    First Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollButton: Button
+    private lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rollButton = findViewById(R.id.btnRoll)
+        diceImage = findViewById(R.id.imageView)
+
+        rollButton.setOnClickListener { roll()}
+
+        // do a roll dice when app starts
+        roll()
+    }
+
+    // the roll function returns a random dice value each time a user taps the button
+
+    private fun roll() {
+        var randomNumber = (1..6).random()
+        when(randomNumber){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        diceImage.contentDescription = randomNumber.toString()
+
+
+    }
+}
